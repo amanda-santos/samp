817115f535314c5e34522a216c59d8b95d74c4cc
AmandaSantos <amanda5652@outlook.com>
2019-04-01 23:18:01 -0300

    conexão ao bd

---
+++ conexao/conecta.php
@@ -0,0 +1,14 @@
+﻿<?php
+	session_start();
+	//Parâmetros de conexão
+	$servername = "200.18.128.51";
+	$username = "samp";
+	$password = "samp2019";
+	$dbname = "samp";
+	// Cria a conexão
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check a conexão
+	if ($conn->connect_error) {
+		die("Falha na conexão: " . $conn->connect_error);
+	}
+?>
