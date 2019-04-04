8700ef25d4fb3a22d1eef44849fc585159a69e7e
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-04-04 11:46:56 -0300

    login funcionando

--- login.php
+++ login.php
@@ -1,31 +1,37 @@
 <?php 
 include("conexao/conecta.php"); //incluir arquivo com conexão ao banco de dados
 
-$conecta = mysqli_connect($servername, $username, $password) or die
-("Sem conexão com o servidor");
-$select=mysqli_select_db($conecta,$dbname) or die("Sem acesso ao DB, Entre em 
-contato com o Administrador");
-session_start();
+//$conecta = mysqli_connect($servername, $username, $password) or die("Sem conexão com o servidor"); // isso já é feito pelo conexao/conecta
+//$select=mysqli_select_db($conecta,$dbname) or die("Sem acesso ao DB, Entre em contato com o Administrador"); // isso já é feito pelo conexao/conecta
 
-$login = isset($_POST['usuario']) ? $_POST['usuario'] : '';
-$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
-$senhacrip = base64_encode($senha);
+//session_start();
 
-$search = "SELECT * FROM `tb_usuario` WHERE `usuario` = '$login' AND `senha`= '$senhacrip'";
-$result = mysqli_query($conecta,$search);
+if (isset($_POST["entrar"])) {
 
+	$usuario = trim(strip_tags($_POST['usuario'])); //trim remove espaços a mais e strip_tags remove tags html e outros códigos
+	$senha = trim(strip_tags($_POST['senha']));
+	$senhaCrip = base64_encode($senha);
 
-if(mysqli_num_rows ($result) > 0 )
-{
-	
-	$_SESSION['login'] = $login;
-	$_SESSION['senha'] = $senha;
-header('location:dashboard.html');
-}
-else{
-  unset ($_SESSION['login']);
-  unset ($_SESSION['senha']);
-  header('location:criarConta.php');
+	$sql = "SELECT * FROM tb_usuario WHERE usuario = '" . $usuario . "' AND senha = '" . $senhaCrip. "';";
+	$resultado = $conn->query($sql);
+
+	if ($resultado->num_rows > 0) { //SE O USUÁRIO E SENHA FOREM VÁLIDOS
+		while ($row = $resultado->fetch_assoc()) {
+			$_SESSION['usuario'] = $linha['usuario'];
+			$_SESSION['senha'] = $linha['senha'];
+			$_SESSION['nome'] = $linha['nome'];
+			$_SESSION['sobrenome'] = $linha['sobrenome'];
+			header('location:dashboard.html');
+		}
+	}else{
+
+		unset ($_SESSION['login']);
+		unset ($_SESSION['senha']);
+		echo "<script>alert('Erro no login. Tente novamente.');</script>";
+		echo "<script>window.location = 'javascript:window.history.go(-1)';</script>";
+
+	}
+
 }
   
 ?>
