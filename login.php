87b1e0576c02314e00802586adb12afe5923b55e
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-04-04 14:41:55 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,8 +4,6 @@ include("conexao/conecta.php"); //incluir arquivo com conexão ao banco de dados
 //$conecta = mysqli_connect($servername, $username, $password) or die("Sem conexão com o servidor"); // isso já é feito pelo conexao/conecta
 //$select=mysqli_select_db($conecta,$dbname) or die("Sem acesso ao DB, Entre em contato com o Administrador"); // isso já é feito pelo conexao/conecta
 
-session_start();
-
 if (isset($_POST["entrar"])) {
 
 	$usuario = trim(strip_tags($_POST['usuario'])); //trim remove espaços a mais e strip_tags remove tags html e outros códigos
@@ -16,13 +14,8 @@ if (isset($_POST["entrar"])) {
 	$resultado = $conn->query($sql);
 
 	if ($resultado->num_rows > 0) { //SE O USUÁRIO E SENHA FOREM VÁLIDOS
-		while ($row = $resultado->fetch_assoc()) {
-			$_SESSION['usuario'] = $linha['usuario'];
-			$_SESSION['senha'] = $linha['senha'];
-			$_SESSION['nome'] = $linha['nome'];
-			$_SESSION['sobrenome'] = $linha['sobrenome'];
-			header('location:dashboard.php');
-		}
+		$_SESSION['usuario'] = $usuario;
+		header('location:dashboard.php');
 	}else{
 
 		unset ($_SESSION['login']);
