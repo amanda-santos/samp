fc13625d8571cf7e3def1f16e15ea781508037d1
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-04-04 16:00:00 -0300

    Update inserirConta.php

--- inserirConta.php
+++ inserirConta.php
@@ -3,7 +3,7 @@
 
 	if (isset($_POST["cadastrar"])) {
 
-		$sql = "SELECT * FROM tb_usuario WHERE usuario='".$_POST["nome"]."'";
+		$sql = "SELECT * FROM tb_usuario WHERE usuario='".$_POST["usuario"]."'";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) { // se achar algum registro
 			echo "<script>alert('Esse nome de usuário já existe.');</script>";
@@ -22,33 +22,20 @@
 				$senhaDigitada = $_POST["senha"];
 				$senha = base64_encode($senhaDigitada); //codifica a senha
 
-				//recebe os valores enviados pelo formulário
-				$file = $_FILES['img'];
+				//define o comando sql para inserção do nome da imagem no banco de dados
+				$SQL = "INSERT INTO tb_usuario (usuario, senha, nome, sobrenome, email, ativo) VALUES ('".$usuario."','".$senha."','".$nome."','".$sobrenome."','" . $email."',1)";
 
-				//permite debugar e ver o que foi enviado
-				//var_dump($file);
+				if ($conn->query($SQL) === TRUE){
+					//verifica se o comando foi executado com sucesso
+					echo "<script>alert('Sua conta foi criada com sucesso! Faça a ativação para começar a usar a ferramenta.');</script>";
+					echo "<script>window.location = 'enviarEmailAtivacao.php?email='" . $email . "';</script>";
 
-				//conta quantas imagens foram enviadas
-				$numFile = count(array_filter($file['name']));
-
-				if ($numFile<=0) {
-					include("conexao/conecta.php"); //incluir arquivo com conexão ao banco de dados
-
-					//define o comando sql para inserção do nome da imagem no banco de dados
-					$SQL = "INSERT INTO tb_usuario (usuario, senha, nome, sobrenome, email, ativo) VALUES ('".$usuario."','".$senha."','".$nome."','".$sobrenome."','" . $email."',0)";
-
-					if ($conn->query($SQL) === TRUE){
-						//verifica se o comando foi executado com sucesso
-						echo "<script>alert('Sua conta foi criada com sucesso! Faça a ativação para começar a usar a ferramenta.');</script>";
-						echo "<script>window.location = 'enviarEmailAtivacao.php?email='" . $email . "';</script>";
-
-					}else{
-						//mensagem exibida caso ocorra algum erro na execução do comando sql
-						echo "<script>alert('Erro ao criar a conta!');</script>";
-						echo "Erro: ". $SQL. "<br>" . $conn->error;
-					}
-				}	
-			}
+				}else{
+					//mensagem exibida caso ocorra algum erro na execução do comando sql
+					echo "<script>alert('Erro ao criar a conta!');</script>";
+					echo "Erro: ". $SQL. "<br>" . $conn->error;
+				}
+			}	
 		}
 	}
 ?>
