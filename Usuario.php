da3e466cd279551586c1d59e8b837a77860a9bb0
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-04-11 14:31:04 -0300

    ativar conta 2

--- Usuario.php
+++ Usuario.php
@@ -8,7 +8,7 @@
 			
 		}
 
-		public function Usuario($nome, $sobrenome, $usuario, $email, $senha){
+		public function Usuario(){
 			$this->nome = $nome;
 			$this->sobrenome = $sobrenome;
 			$this->usuario = $usuario;
@@ -17,27 +17,27 @@
 			$this->ativo = 0;
 		}
 
-		public function inserirUsuario($conn){
-			$sql = "SELECT * FROM tb_usuario WHERE usuario = '".$this->usuario."';";
+		public function inserirUsuario($nome, $sobrenome, $usuario, $email, $senha, $conn){
+			$sql = "SELECT * FROM tb_usuario WHERE usuario = '".$usuario."';";
 			$result = $conn->query($sql);
 			if ($result->num_rows > 0) { // se achar algum registro
 				echo "<script>alert('Esse nome de usuário já existe.');</script>";
 				echo "<script>window.location = 'javascript:window.history.go(-1)';</script>";
 			}else{
-				$SQL = "SELECT * FROM tb_usuario WHERE email = '". $this->email . "';";
+				$SQL = "SELECT * FROM tb_usuario WHERE email = '". $email . "';";
 				$result = $conn->query($SQL);
 				if ($result->num_rows > 0) { // se achar algum registro
 					echo "<script>alert('Já existe uma conta cadastrada com esse e-mail.');</script>";
 					echo "<script>window.location = 'javascript:window.history.go(-1)';</script>";
 				} else {
 				//define o comando sql para inserção
-					$SQL = "INSERT INTO tb_usuario (usuario, senha, nome, sobrenome, email, ativo) VALUES ('".$this->usuario."','".$this->senha."','".$this->nome."','".$this->sobrenome."','".$this->email."',".$this->ativo.")";
+					$SQL = "INSERT INTO tb_usuario (usuario, senha, nome, sobrenome, email, ativo) VALUES ('".$usuario."','".$senha."','".$nome."','".$sobrenome."','".$email."',0)";
 
 					if ($conn->query($SQL) === TRUE){
 						//verifica se o comando foi executado com sucesso
 						//$_SESSION['usuario'] = $this->$usuario;
 						//$_SESSION['nome'] = $this->$nome;
-						echo "<script>window.location = 'enviarEmailAtivacao.php?email=$this->email';</script>";
+						echo "<script>window.location = 'enviarEmailAtivacao.php?email=$email';</script>";
 					}else{
 						//mensagem exibida caso ocorra algum erro na execução do comando sql
 						echo "<script>alert('Erro ao criar a conta!');</script>";
@@ -54,8 +54,11 @@
 			if ($conn->query($SQL) === TRUE){
 				//verifica se o comando foi executado com sucesso
 				echo "<script>alert('Sua conta foi ativada com sucesso!');</script>";
-				$_SESSION['usuario'] = $this->usuario;
-				$_SESSION['nome'] = $this->nome;
+				$_SESSION['usuario'] = $usuario;
+				$_SESSION['nome'] = $nome;
+				$_SESSION['sobrenome'] = $sobrenome;
+				$_SESSION['email'] = $email;
+				$_SESSION['senha'] = $senha;
 				echo "<script>window.location = 'dashboard.php';</script>";
 			}else{
 				//mensagem exibida caso ocorra algum erro na execução do comando sql
