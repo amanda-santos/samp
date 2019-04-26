f85cc063bf87e630e7bae58f465451f796538f7c
CarlaPonciano <49000683+CarlaPonciano@users.noreply.github.com>
2019-04-26 12:49:36 -0300

    Update projetoModel.php

--- model/projetoModel.php
+++ model/projetoModel.php
@@ -8,7 +8,7 @@
 			$SQL = "INSERT INTO projeto (nome, descricao, id) VALUES ('".$nome."','".$descricao."','".$id."');";
 			if ($conn->query($SQL) === TRUE){
 				//verifica se o comando foi executado com sucesso
-				$SQL = "INSERT INTO usuario_projeto (scrum_master, usuario_usuario, projeto_id) VALUES (1,'CarlaPon','".$id."');";
+				$SQL = "INSERT INTO usuario_projeto (scrum_master, usuario_usuario, projeto_id) VALUES (1,'".$_SESSION['usuario']."','".$id."');";
 				if ($conn->query($SQL) === TRUE){
 					echo "<script>alert('Projeto cadastrado com sucesso! Código do projeto: $id');</script>";
 					echo "<script>window.location = '../view/projetos.php';</script>";
