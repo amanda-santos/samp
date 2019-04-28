1a0a296d9da165039e2aaaa4d85df724bdf45f8a
AmandaSantos <amanda5652@outlook.com>
2019-04-27 21:27:07 -0300

    inativarUsuario e altSenha antigos apagados

--- inativarUsuario.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include("conexao/conecta.php"); //incluir arquivo com conexão ao banco de dados
-	if (isset($_SESSION["usuario"])) { //SE EXISTIR AUTENTICAÇÃO
-
-	} else {
-		echo "<script>window.location = 'index.php';</script>";
-	}
-?>
