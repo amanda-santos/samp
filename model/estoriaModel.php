ea52e5db3f06ad0344b62c49ae15bc6e37ac27da
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-05-02 16:02:24 -0300

    Update estoriaModel.php

--- model/estoriaModel.php
+++ model/estoriaModel.php
@@ -8,7 +8,7 @@
 			if ($conn->query($SQL) === TRUE){
 				//verifica se o comando foi executado com sucesso
 				echo "<script>alert('Estoria cadastrada com sucesso!');</script>";
-				echo "<script>window.location = '../view/productBacklog.php';</script>";
+				echo "<script>window.location = '../view/productBacklog.php?id=$projeto_id';</script>";
 			}else{
 				//mensagem exibida caso ocorra algum erro na execução do comando sql
 				echo "<script>alert('Erro ao cadastrar estória!');</script>";
