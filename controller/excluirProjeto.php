bbf4c2f8fcdcee14c28f9ce4dd4bd9dc48420c15
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-05-02 16:01:44 -0300

    Update excluirProjeto.php

--- controller/excluirProjeto.php
+++ controller/excluirProjeto.php
@@ -1,5 +1,5 @@
 <?php
-	require_once '../model/projetos.php';
+	require_once '../model/Projeto.php';
 	require_once '../model/conexao/conecta.php';
 
 	if (isset($_SESSION["usuario"])) { //SE EXISTIR AUTENTICAÇÃO
@@ -9,3 +9,4 @@
 		echo "<script>window.location = '../view/index.html';</script>";
 	}
 ?>
+
