68bf2bb402a8586476693e4b6f1957063b50a8ee
AmandaSantos <amanda5652@outlook.com>
2019-05-12 16:51:41 -0300

    diagramas atualizados

--- model/Projeto.php
+++ model/Projeto.php
@@ -1,12 +1,13 @@
 <?php
 	class Projeto{
-		private $nome, $descricao, $id, $scrum_master;
+		private $nome, $descricao, $id, $scrum_master, $product_backlog;
 
-		public function __construct($paramNome='', $paramDesc='', $paramId='', $paramScrumMaster=''){
+		public function __construct($paramNome='', $paramDesc='', $paramId='', $paramScrumMaster='', $paramProductBacklog=''){
 		    $this->nome = $paramNome;
 		    $this->descricao = $paramDesc;
 		    $this->id = $paramId;
 		    $this->scrum_master = $paramScrumMaster;
+		    this->product_backlog = $paramProductBacklog;
 		}
 
 		public function setNome($nome){
@@ -23,6 +24,10 @@
 			$this->scrum_master=$scrum_master;
 		}
 
+		public function setProduct_Backlog($product_backlog){
+			$this->product_backlog=$product_backlog;
+		}
+
 		public function getNome(){
 			return $this->nome;
 		}
@@ -34,6 +39,9 @@
 		}
 		public function getScrumMaster(){
 			return $this->scrum_master;
+		}
+		public function getProductBacklog(){
+			return $this->product_backlog;
 		}
 	}
 ?>
