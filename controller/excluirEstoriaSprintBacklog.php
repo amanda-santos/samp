77c1d10c8674ea0b65f229babe5ba2c883b4e87e
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-05-16 10:18:40 -0300

    excluir estória do sprint backlog funcionando

--- controller/excluirEstoriaSprintBacklog.php
+++ controller/excluirEstoriaSprintBacklog.php
@@ -4,7 +4,7 @@
 
 	if (isset($_SESSION["usuario"])) { //SE EXISTIR AUTENTICAÇÃO
 		$estoriaDAO = new estoriaDAO();
-		$estoriaDAO->excluirEstoriaSprintBacklog($_GET['estoria']);
+		$estoriaDAO->excluirEstoriaSprintBacklog($_GET['idEstoria'],$_GET['idProjeto']);
 	} else {
 		echo "<script>window.location = '../view/index.html';</script>";
 	}
