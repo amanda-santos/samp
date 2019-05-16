e18ec707f9b6248fa54c8d7ab5b82716bd724da4
CarlaPonciano <49000683+CarlaPonciano@users.noreply.github.com>
2019-05-16 14:39:16 -0300

    Update cadastrarEstoriaSprintBacklog.php

--- controller/cadastrarEstoriaSprintBacklog.php
+++ controller/cadastrarEstoriaSprintBacklog.php
@@ -3,13 +3,15 @@
   require_once '../model/estoriaDAO.php';
   //Criando e Instanciando o objeto
   if (isset($_POST["cadastrar_estoria"])){
+    
     $idProjeto = $_GET["idProjeto"];
+    $idEstoria = $_GET["id_estoria"];
 
     $estoriaDAO = new estoriaDAO();
     $nivel_dificuldade = $_POST['nivel_dificuldade'];
 
-    $estoriaDAO->persistirEstoriaSprintBacklog($idProjeto, $_POST['id_estoria_cadastrada'], $nivel_dificuldade, $_POST['duracao']);
-    $estoriaDAO->persistirEstoriaUsuario($idProjeto, $_POST['id_responsavel'], $_POST['id_estoria_cadastrada']);
+    $estoriaDAO->persistirEstoriaSprintBacklog($idProjeto, $idEstoria, $nivel_dificuldade, $_POST['duracao']);
+    $estoriaDAO->persistirEstoriaUsuario($idProjeto, $_POST['id_responsavel'], $idEstoria);
     exit;
   }
 ?>
