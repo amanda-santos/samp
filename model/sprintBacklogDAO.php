200179d3325a9a779985961481fbf1c48334b0bc
amanda-santos <49074930+amanda-santos@users.noreply.github.com>
2019-05-16 10:18:01 -0300

    excluir estória do sprint backlog funcionando

--- model/sprintBacklogDAO.php
+++ model/sprintBacklogDAO.php
@@ -4,10 +4,10 @@
 
 	class sprintBacklogDAO{
 
-		function selecionarSprintBacklog($estoria_id){
+		function selecionarSprintBacklog($projeto_id){
 			include 'conexao/conecta.php';
 			//define o comando sql para seleção
-			$SQL = "SELECT * FROM estoria AS e JOIN nivel_dificuldade AS nd ON e.NivelDificuldade_id = nd.id JOIN situacao AS s ON e.Situacao_id = s.id WHERE Projeto_id = '".$estoria_id."' AND sprint_backlog = 1;";
+			$SQL = "SELECT e.id, e.nome, e.descricao, e.duracao, nd.nivel_dificuldade, s.situacao FROM estoria AS e JOIN nivel_dificuldade AS nd ON e.NivelDificuldade_id = nd.id JOIN situacao AS s ON e.Situacao_id = s.id WHERE Projeto_id = '".$projeto_id."' AND sprint_backlog = 1;";
 			$result = $conn->query($SQL);
     		if ($result->num_rows > 0) { // Exibindo cada linha retornada com a consulta
 				$estorias = new ArrayObject();
