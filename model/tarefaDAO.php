aae4ab7c5ee5f4b01f2ee34ef4e168e7dbe0266a
CarlaPonciano <si.carla.ponciano@outlook.com>
2019-05-26 15:02:11 -0300

    cadastro de tarefas

---
+++ model/tarefaDAO.php
@@ -0,0 +1,21 @@
+<?php
+	require_once 'Tarefa.php';
+
+	class tarefaDAO{
+
+		public function persistirTarefa($nome, $estoria_id, $projeto_id){
+			include 'conexao/conecta.php';
+
+			$SQL = "INSERT INTO tarefa (nome, Situacao_id, Estoria_id) VALUES ('".$nome."',1,'".$estoria_id."');";
+			if ($conn->query($SQL) === TRUE){
+				echo "<script>alert('Tarefa cadastrada com sucesso!');</script>";
+				echo "<script>window.location = '../controller/dashboardProjeto.php?id=$projeto_id';</script>";
+			}else{
+				echo "<script>alert('Erro ao cadastrar tarefa!');</script>";
+				echo "Erro: ". $SQL. "<br>" . $conn->error;
+			}
+			$conn->close();
+		}
+
+	}
+?>
